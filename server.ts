a0e32f42a9cca6f6040f0a5b4bb1d8c2d6674615
Amitav Roy <amitav.roy@focalworks.in>
2020-01-09 13:56:19 +0530

    Working on sequalize for ORM integration with MySQL

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import App from './src/app'
 import * as dotenv from 'dotenv'
+import {Sequelize} from 'sequelize'
 
 const config = dotenv.config()
 
@@ -7,5 +8,20 @@ const app = new App({
   port: process.env.APP_PORT,
   middlewares: []
 })
+
+const sequelize = new Sequelize('syncup', 'newuser', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  logging: console.log
+})
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Database connection success')
+  })
+  .catch(error => {
+    console.log('Error connecting to the database:', error.parent.sqlMessage)
+    // Need to redirect to error page
+  })
 
 app.listen()
